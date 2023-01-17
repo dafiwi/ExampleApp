092e470705ab5fc2cfd59621310d127d30b4b986
dafiwi <94173804+dafiwi@users.noreply.github.com>
2023-01-17 18:09:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Example App");
+Console.WriteLine("\nThis is just an Example Application for Testing Purposes");
+Console.WriteLine("\n\nThe App will count to 3 and then close itself.");
+
+Thread.Sleep(1000);
+Console.WriteLine("\n1...");
+Thread.Sleep(1000);
+Console.WriteLine("2...");
+Thread.Sleep(1000);
+Console.WriteLine("3...");
+Thread.Sleep(1000);
+Console.WriteLine("\nQuit App");
+Thread.Sleep(1000);
